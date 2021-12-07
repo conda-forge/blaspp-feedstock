fbfbb83a92d6935eca376f3c087e28986196060c
Axel Huebl <axel.huebl@plasma.ninja>
2021-12-07 01:48:53 -0800

    Set: BLAS_LIBRARIES
    Same as we do in Spack. Avoids all heuristics and avoids that, e.g.,
    macOS picks up the accelerate BLAS bindings.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# work-around for https://bitbucket.org/icl/blaspp/issues/13
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-    export CMAKE_ARGS="${CMAKE_ARGS} -Dblas_config_cache=${PREFIX}/lib/libblas${SHLIB_EXT} -DBLAS_LIBRARIES=${PREFIX}/lib/libblas${SHLIB_EXT}"
-    export CMAKE_ARGS="${CMAKE_ARGS} -Dcblas_config_cache=${PREFIX}/lib/libblas${SHLIB_EXT} -Dblaspp_cblas_libraries=-lcblas"
-fi
-
 cmake -S . -B build             \
     -DCMAKE_INSTALL_LIBDIR=lib        \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}  \
@@ -16,6 +10,7 @@ cmake -S . -B build             \
     -Duse_cmake_find_blas=ON    \
     -Duse_openmp=ON             \
     -Dgpu_backend=none          \
+    -DBLAS_LIBRARIES=${PREFIX}/lib/libblas${SHLIB_EXT} \
     ${CMAKE_ARGS}
 
 cmake --build build --parallel ${CPU_COUNT}
